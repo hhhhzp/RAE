51a96c07069afcf8877576d75f7154c663d5e972
hhhhzp <523470679@qq.com>
2026-01-22 16:56:27 +0800

    1

---
+++ install.sh
@@ -0,0 +1,9 @@
+conda create -n rae python=3.10 -y
+conda activate rae
+pip install uv
+
+# Install PyTorch 2.8.0 with CUDA 12.9 # or your own cuda version
+uv pip install torch==2.8.0 torchvision torchaudio --index-url https://mirrors.cloud.tencent.com/pypi/simple
+
+# Install other dependencies
+uv pip install -r requirements.txt --index-url https://mirrors.cloud.tencent.com/pypi/simple
