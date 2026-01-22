4f96b9ad28e343e77b81110b7dafe0ef3fbb6f2a
hhhhzp <523470679@qq.com>
2026-01-22 17:05:24 +0800

    1

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@ conda create -n rae python=3.10 -y
 
 unset https_proxy
 unset http_proxy
-conda run -n rae pip install uv \
+conda activate rae
+pip install uv \
   --index-url https://mirrors.cloud.tencent.com/pypi/simple
-conda run -n rae uv pip install torch==2.8.0 torchvision torchaudio \
+uv pip install torch==2.8.0 torchvision torchaudio \
   --index-url https://mirrors.cloud.tencent.com/pypi/simple
-conda run -n rae uv pip install -r requirements.txt \
+uv pip install -r requirements.txt \
   --index-url https://mirrors.cloud.tencent.com/pypi/simple
